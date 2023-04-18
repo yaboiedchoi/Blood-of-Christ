b761bcab48c38b4db0bd94687a81f852e34174e2
Vaibhavy Darshan <vd5443@rit.edu>
2023-04-18 13:14:32 -0400

    comment

--- Game1.cs
+++ Game1.cs
@@ -20,6 +20,7 @@ namespace Blood_of_Christ
     {
         // KNOWN BUGS:
         // fireball does not reset location when restarting game
+        // player buffers at some point
         // TODO: Add a pause menu
         // NOTE: we should probably move all of these to manager classes later
         private GraphicsDeviceManager _graphics;
